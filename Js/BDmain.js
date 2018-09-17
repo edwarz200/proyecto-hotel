46bf35c7015f4b2ce5e2dc7c7b8f2395c725d5fb
unknown <edwarz200@hotmail.com>
2018-09-17 09:50:40 -0500

    first commit

---
+++ Js/BDmain.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCYOQJ4-y7fKB-d7eA2-fwYkTh9FZeWBGk",
+    authDomain: "sena-hotel-bd.firebaseapp.com",
+    databaseURL: "https://sena-hotel-bd.firebaseio.com",
+    projectId: "sena-hotel-bd",
+    storageBucket: "sena-hotel-bd.appspot.com",
+    messagingSenderId: "504296591453"
+  };
+  firebase.initializeApp(config);
