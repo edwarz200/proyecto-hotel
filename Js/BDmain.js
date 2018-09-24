69e8eb35f971bec3ce8b67a35934e3c0b5325904
unknown <edwarz200@hotmail.com>
2018-09-24 03:58:47 -0500

    BD

--- Js/BDmain.js
+++ Js/BDmain.js
@@ -1,88 +1,141 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCYOQJ4-y7fKB-d7eA2-fwYkTh9FZeWBGk",
-    authDomain: "sena-hotel-bd.firebaseapp.com",
-    databaseURL: "https://sena-hotel-bd.firebaseio.com",
-    projectId: "sena-hotel-bd",
-    storageBucket: "sena-hotel-bd.appspot.com",
-    messagingSenderId: "504296591453"
+var config = {
+  apiKey: "AIzaSyCYOQJ4-y7fKB-d7eA2-fwYkTh9FZeWBGk",
+  authDomain: "sena-hotel-bd.firebaseapp.com",
+  databaseURL: "https://sena-hotel-bd.firebaseio.com",
+  projectId: "sena-hotel-bd",
+  storageBucket: "sena-hotel-bd.appspot.com",
+  messagingSenderId: "504296591453"
 
 
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
+// function writeHotelData(telhot, correohot, histo) {
+//   firebase.database().ref('Hotel/' + userId).set({
+//     telefono: telhot,
+//     email: correohot,
+//     historia: histo,
+//   });
+// }
 
-  function writeHotelData(telhot,correohot,histo) {
-    firebase.database().ref('Hotel/' + userId).set({
-      telefono: telhot,
-      email: correohot,
-      historia : histo,
-    });
-  }
+// function getHotelData(telhot, correohot, histo) {
+//   firebase.database().ref('Hotel/' + userId).get({
+//     telefono: telhot,
+//     email: correohot,
+//     historia: histo,
+//   });
+// }
 
-  function writeHabitacionesData(tipohab,capacidad,valorhab,contenido,fotos,estado) {
-    firebase.database().ref('Hotel/Habitaciones' + userId).set({
-      tipo_hab: tipohab,
-      capacidad: capacidad,
-      precio : valorhab,
-      contenido_hab: contenido,
-      fotografias_hab: fotos,
-      estado_hab: estado
-    });
-  }
+// function writeHabitacionesData(tipohab, capacidad, valorhab, contenido, fotos, estado) {
+//   firebase.database().ref('Hotel/Habitaciones' + userId).set({
+//     tipo_hab: tipohab,
+//     capacidad: capacidad,
+//     precio: valorhab,
+//     contenido_hab: contenido,
+//     fotografias_hab: fotos,
+//     estado_hab: estado
+//   });
+// }
 
-  function writeServiciosData(nombreserv,descripcionserv,imagenserv,horarioaten, valorser) {
-    firebase.database().ref('Hotel/Servicios' + userId).set({
-      nombre_serv: nombreserv,
-      descripcion_serv: descripcionserv,
-      Fotografias_serv : imagenserv,
-      horario_atencion: horarioaten,
-      valor_serv: valorser
-    });
-  }
+// function getHabitacionesData(tipohab, capacidad, valorhab, contenido, fotos, estado) {
+//   firebase.database().ref('Hotel/Habitaciones' + userId).get({
+//     tipo_hab: tipohab,
+//     capacidad: capacidad,
+//     precio: valorhab,
+//     contenido_hab: contenido,
+//     fotografias_hab: fotos,
+//     estado_hab: estado
+//   });
+// }
 
-  function writeGaleriaData(fotoshot) {
-    firebase.database().ref('Hotel/Servicios' + userId).set({
-      fotos_hot: fotoshot,
-    });
-  }
+// function writeServiciosData(nombreserv, descripcionserv, imagenserv, horarioaten, valorser) {
+//   firebase.database().ref('Hotel/Servicios' + userId).set({
+//     nombre_serv: nombreserv,
+//     descripcion_serv: descripcionserv,
+//     Fotografias_serv: imagenserv,
+//     horario_atencion: horarioaten,
+//     valor_serv: valorser
+//   });
+// }
 
-  function writeResevacionesData(fechain,fechafi) {
-    firebase.database().ref('Hotel/Reservaciones' + userId).set({
-      fecha_in: fechain,
-      fecha_fin: fechafi
-    });
-  }
+// function getServiciosData(nombreserv, descripcionserv, imagenserv, horarioaten, valorser) {
+//   firebase.database().ref('Hotel/Servicios' + userId).get({
+//     nombre_serv: nombreserv,
+//     descripcion_serv: descripcionserv,
+//     Fotografias_serv: imagenserv,
+//     horario_atencion: horarioaten,
+//     valor_serv: valorser
+//   });
+// }
 
-  function writeClientesData(dnicli,nomcli,telecli,corcli) {
-    firebase.database().ref('Hotel/Reservaciones/Clientes' + userId).set({
-      dni_cli: dnicli,
-      nombre_cli: nomcli,
-      telefono_cli: telecli,
-      correo_cli: corcli
-    });
-  }
+// function writeGaleriaData(fotoshot) {
+//   firebase.database().ref('Hotel/Servicios' + userId).set({
+//     fotos_hot: fotoshot,
+//   });
+// }
 
-  function writeReserva_habitacionesData(tipohabr,capacihabr,valorhabr,fotosr,estadohabr) {
-    firebase.database().ref('Hotel/Reservaciones/habitaciones' + userId).set({
-      tipo_hab_r: tipohabr,
-      capacidad_habitacion_r: capacihabr,
-      valor_hab_r: valorhabr,
-      fotos_hab_r: fotosr,
-      estado_hab_r: estadohabr
-    });
-  }
-  
-  function writePerfilData(dniper, nombreper, apellper, telper,fechnac, dirper, correoper,fotoper,tipousuario) {
-    firebase.database().ref('Hotel/Perfiles' + userId).set({
-      dni_perf: dniper,
-      nombre_perf: nombreper,
-      apellido_perf:apellper,
-      telefono_perf: telper,
-      fecha_nacimiento: fechnac,
-      direccion_perf: dirper,
-      correo_perf: correoper,
-      foto_perf:fotoper,
-      tipo_usuario: tipousuario,
-    });
-  }
+// function getGaleriaData(fotoshot) {
+//   firebase.database().ref('Hotel/Servicios' + userId).get({
+//     fotos_hot: fotoshot,
+//   });
+// }
+
+// function writeResevacionesData(fechain, fechafi) {
+//   firebase.database().ref('Hotel/Reservaciones' + userId).set({
+//     fecha_in: fechain,
+//     fecha_fin: fechafi
+//   });
+// }
+
+// function getResevacionesData(fechain, fechafi) {
+//   firebase.database().ref('Hotel/Reservaciones' + userId).get({
+//     fecha_in: fechain,
+//     fecha_fin: fechafi
+//   });
+// }
+
+// function writeClientesData(dnicli, nomcli, telecli, corcli) {
+//   firebase.database().ref('Hotel/Reservaciones/Clientes' + userId).set({
+//     dni_cli: dnicli,
+//     nombre_cli: nomcli,
+//     telefono_cli: telecli,
+//     correo_cli: corcli
+//   });
+// }
+
+// function getReserva_habitacionesData(tipohabr, capacihabr, valorhabr, fotosr, estadohabr) {
+//   firebase.database().ref('Hotel/Reservaciones/habitaciones' + userId).get({
+//     tipo_hab_r: tipohabr,
+//     capacidad_habitacion_r: capacihabr,
+//     valor_hab_r: valorhabr,
+//     fotos_hab_r: fotosr,
+//     estado_hab_r: estadohabr
+//   });
+// }
+
+function writePerfilData(nombreper, apellper, telper, correoper) {
+  const $formLogin = document.querySelector('#form-login')
+  const formData = new FormData($formLogin)
+  var dniper = formData.get('dni');
+  firebase.database().ref('Hotel/Perfiles' + dniper).set({
+    nombre_perf: nombreper,
+    apellido_perf: apellper,
+    telefono_perf: telper,
+    correo_perf: correoper,
+  });
+}
+
+// function getPerfilData(dniper, nombreper, apellper, telper, fechnac, dirper, correoper, fotoper, tipousuario) {
+//   firebase.database().ref('Hotel/Perfiles' + userId).get({
+//     dni_perf: dniper,
+//     nombre_perf: nombreper,
+//     apellido_perf: apellper,
+//     telefono_perf: telper,
+//     fecha_nacimiento: fechnac,
+//     direccion_perf: dirper,
+//     correo_perf: correoper,
+//     foto_perf: fotoper,
+//     tipo_usuario: tipousuario,
+//   });
+// }
--- Js/mained.js
+++ Js/mained.js
@@ -1,28 +1,94 @@
-crearUsuario=()=>{ 
-    autenticacion.createUserWithEmailAndPassword(this.state.username,this.state.password) 
-    .then((success)=>{ 
-    const uniqueId = DeviceInfo.getUniqueID();
-    const miid=autenticacion.currentUser.uid;
-    //AHORA LOS DEMAS DATOS A LA BD @ 
-    
-    this.storeDataUser(this.state.username); 
-    Toast.show(Idioma.t('registerOk'), Toast.LONG);
-    this.props.navigation.navigate('Login') 
+document.addEventListener('DOMContentLoaded', () => {
+    // resgistro
+    var regisname = document.querySelector('.namer')
+    var regisapellido = document.querySelector('.apellir')
+    var regiscorreo = document.querySelector('.correor')
+    var repas = document.querySelector('.recontraseñar')
+    var regisbtnregistro = document.querySelector('.btnrgistro')
+
+    const $formLogin = document.querySelector('#form-login')
+    var body = document.querySelector('body')
+    var num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+    var num5 = 0, num6 = 0;
+
+    repas.addEventListener('keypress', (e) => {
+        tecla = (document.all) ? e.keyCode : e.which;
+        if (tecla == 13) {
+            enviar();
+        }
     })
-    .catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message; 
-    if(errorCode==='auth/invalid-email'){ 
-   
-   Toast.show(Idioma.t('passsecurity'), Toast.LONG); 
-    }else if(errorCode==='auth/weak-password'){
-    Toast.show(Idioma.t('passsecurity'), Toast.LONG); 
-    }else if(errorCode==='auth/email-already-in-use'){
-    Toast.show(Idioma.t('emailused'), Toast.LONG); 
-    }else if(errorCode==='auth/network-request-failed'){
-    Toast.show(Idioma.t('conexionerror'), Toast.LONG); 
-    }else{
-    Toast.show(errorMessage, Toast.LONG);
+    regisbtnregistro.addEventListener('click', enviar = (event) => {
+        const formData = new FormData($formLogin)
+
+        var dniper = formData.get('dni');
+        var nombreper = formData.get('nombre');
+        var apellper = formData.get('apellidos');
+        var telper = formData.get('telefono');
+        var correoper = formData.get('correo');
+        var regiscontraseña = formData.get('pass');
+        var regisrecontraseña = formData.get('repass');
+
+        if (dniper === "") {
+            alert('falta el Documento')
+        } else { num1 = 1 }
+        if (nombreper === "") {
+            alert('falta el nombre')
+        } else { num2 = 1 }
+        if (apellper === "") {
+            alert('faltan los apellidos')
+        } else { num3 = 1 }
+        if (telper === "") {
+            alert('falta el telefono')
+        } else { num4 = 1 }
+        if (correoper === "") {
+            alert('falta el email')
+        } else { num5 = 1 }
+        if (regiscontraseña === "") {
+            alert('falta la contraseña')
+        } else { num6 = 1 }
+
+        if (num1 === 1 & num2 === 1 & num3 === 1 & num4 === 1 & num5 === 1 & num6 === 1) {
+            if (regiscontraseña === regisrecontraseña) {
+                writePerfilData(nombreper, apellper, telper, correoper);
+                alert('Ha sido guardo correctamente')
+                // crearUsuario();
+                location.href ="principalPage.html";
+            } else {
+                alert('Las contraseñas no son iguales')
+
+            }
+        }
+    })
+    // 
+    // 
+    // 
+    crearUsuario = () => {
+        autenticacion.createUserWithEmailAndPassword(this.state.username, this.state.password)
+            .then((success) => {
+                const uniqueId = DeviceInfo.getUniqueID();
+                const miid = autenticacion.currentUser.uid;
+                //AHORA LOS DEMAS DATOS A LA BD @ 
+
+                this.storeDataUser(this.state.username);
+                Toast.show(Idioma.t('registerOk'), Toast.LONG);
+                this.props.navigation.navigate('Login')
+            })
+            .catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode === 'auth/invalid-email') {
+
+                    Toast.show(Idioma.t('passsecurity'), Toast.LONG);
+                } else if (errorCode === 'auth/weak-password') {
+                    Toast.show(Idioma.t('passsecurity'), Toast.LONG);
+                } else if (errorCode === 'auth/email-already-in-use') {
+                    Toast.show(Idioma.t('emailused'), Toast.LONG);
+                } else if (errorCode === 'auth/network-request-failed') {
+                    Toast.show(Idioma.t('conexionerror'), Toast.LONG);
+                } else {
+                    Toast.show(errorMessage, Toast.LONG);
+                }
+            });
     }
-    }); 
-    }
+
+})
